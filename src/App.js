a064717b68d4c7917da7d85ae0298db1f4bf1e5a
chandni161 <chandni16roy@gmail.com>
2022-07-17 21:49:23 +0530

    Button Component Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Fragment } from 'react';
 import './App.css';
+import Button from './components/Button/Button';
 
 export const App = () => {
-    return <div className='container'>Learning React</div>;
+    const btn1Hander = () => {
+        console.log('btn 1');
+    };
+    const btn2Hander = () => {
+        console.log('btn 2');
+    };
+    const btn3Hander = () => {
+        console.log('btn 3');
+    };
+    return (
+        <Fragment>
+            <div className='container'>Learning React</div>;
+            <Button onClick={btn1Hander}>Prev</Button>
+            <Button onClick={btn2Hander}>Next</Button>
+            <Button onClick={btn3Hander}>Submit</Button>
+        </Fragment>
+    );
 };
 
 export default App;
